c294d7c41fbdbb2ed4e2f69cdc74bc17c9498813
SoohoLee <hyeri0603@naver.com>
2023-04-16 22:18:12 +0900

    hello

---
+++ src/main/java/study/querydsl/entity/Hello.java
@@ -0,0 +1,14 @@
+package study.querydsl.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter @Setter
+public class Hello {
+
+    @Id @GeneratedValue
+    private Long id;
+}
